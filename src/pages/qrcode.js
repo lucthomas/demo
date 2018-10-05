25cb24cd9d66467a346ad6f91cff41dfb639fae2
Luc Thomas <luc.thomas@live.com>
2018-10-05 03:59:19 +0200

    Implement QR Code

--- src/pages/qrcode.js
+++ src/pages/qrcode.js
@@ -5,8 +5,19 @@ import { Helmet } from 'react-helmet';
 //Import withAuth function
 import withAuth from './../hoc/withAuth';
 
-class QRCodeViewer extends React.Component{
+import QRCode from 'qrcode-react';
 
+class QRCodeViewer extends React.Component{
+	
+	constructor(props) {
+        super(props);
+        this.state = { text: "https://engify.network" };
+    }
+
+    _onChange(event) {
+        this.setState({ text: event.target.value });
+	}
+	
 	head(){
 		return(
 			<Helmet>
@@ -23,13 +34,25 @@ class QRCodeViewer extends React.Component{
 				{this.head()}
 				<h1>QR Code</h1>
 				<p>My QR code generator</p>
+				<div className="application">
+				<input type="text" 
+				className="qrcode-input" 
+				onChange={this._onChange.bind(this)}
+				value={this.state.text}
+				placeholder= "input string" /><br /><br />
+				<QRCode className="qrcode" 
+				value= {this.state.text}
+				size={250} fgColor="black" 
+				bgColor="white" 
+				logo="https://media.licdn.com/dms/image/C560BAQEvDLztsYOa8A/company-logo_200_200/0?e=2159024400&v=beta&t=DYueWAhZIaWlLnPjFvRCecCQXh-LC3WmPkIPj3cNhAQ" />
+				</div>
 
 			</div>
 		)
 	}
 }
 
-//Export class QRCode
+//Export class QRCodeViewer
 //Restrict access to this page if not logged in
 
 export default withAuth(session => session && session.getCurrentUser)(QRCodeViewer);
